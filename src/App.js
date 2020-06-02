02a911c02d12f67de97ba1e1188b2a82e13e76ae
Stephanie Fashagba <stephanie.fashagba@thoughtworks.com>
2020-06-02 10:41:20 +0100

    adds box and gives it an outline

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import * as THREE from "three";
 
 function App() {
+  const pastelCream = "#f1e6c4";
   let scene, camera, renderer;
 
   function init() {
     scene = new THREE.Scene();
-    scene.background = new THREE.Color("");
+    scene.background = new THREE.Color(pastelCream);
     let container = document.getElementById("container");
 
     camera = new THREE.PerspectiveCamera(
@@ -17,15 +17,23 @@ function App() {
       0.1,
       100
     );
-    camera.position.x = -4;
-    camera.position.z = 4;
-    camera.position.y = 2;
+    camera.position.x = 0;
+    camera.position.z = 60;
+    camera.position.y = 0;
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.outputEncoding = THREE.sRGBEncoding;
     container.appendChild(renderer.domElement);
+
+    let geometry = new THREE.BoxGeometry(3, 1, 1, 3, 3);
+    let edges = new THREE.EdgesGeometry(geometry);
+    let line = new THREE.LineSegments(
+      edges,
+      new THREE.LineBasicMaterial({ color: "black" })
+    );
+    scene.add(line);
 
     window.addEventListener("resize", onWindowResize, false);
   }
