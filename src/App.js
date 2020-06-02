87976396e2ffbc92e0ceccaef362a12afd51963b
Stephanie Fashagba <stephanie.fashagba@thoughtworks.com>
2020-06-02 11:44:00 +0100

    adds left, right, forward movements with acceleration

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import * as THREE from "three";
 
 function App() {
   const pastelCream = "#f1e6c4";
-  let scene, camera, renderer;
+  const ARROW_UP = "ArrowUp";
+  const ARROW_LEFT = "ArrowLeft";
+  const ARROW_RIGHT = "ArrowRight";
+  const STARTING_SPEED = "startingSpeed";
+  let scene, camera, renderer, direction;
 
   function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color(pastelCream);
     let container = document.getElementById("container");
+    document.onkeydown = setDirectionOnEventKeyPress;
 
     camera = new THREE.PerspectiveCamera(
-      50,
+      1,
       window.innerWidth / window.innerHeight,
-      0.1,
-      100
+      0.01,
+      10000
     );
     camera.position.x = 0;
-    camera.position.z = 60;
+    camera.position.z = 600;
     camera.position.y = 0;
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -27,7 +32,7 @@ function App() {
     renderer.outputEncoding = THREE.sRGBEncoding;
     container.appendChild(renderer.domElement);
 
-    let geometry = new THREE.BoxGeometry(3, 1, 1, 3, 3);
+    let geometry = new THREE.BoxGeometry(1, 1, 1, 3, 3);
     let edges = new THREE.EdgesGeometry(geometry);
     let line = new THREE.LineSegments(
       edges,
@@ -41,7 +46,7 @@ function App() {
   useEffect(() => {
     init();
     animate();
-  }, []);
+  });
 
   function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -52,6 +57,37 @@ function App() {
   function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    handleMovementAnimation();
+  }
+
+  const handleMovementAnimation = () => {
+    let forwardSpeed = 2;
+    let noSpeedChange = 0;
+    let sideSpeed = 0.1;
+
+    switch (direction) {
+      case undefined:
+        camera.position.z -= noSpeedChange;
+        break;
+      case ARROW_UP:
+        forwardSpeed += 1;
+        camera.position.z -= forwardSpeed;
+        break;
+      case ARROW_LEFT:
+        camera.position.x += sideSpeed;
+        camera.position.z -= forwardSpeed;
+        break;
+      case ARROW_RIGHT:
+        camera.position.x -= sideSpeed;
+        camera.position.z -= forwardSpeed;
+        break;
+      default:
+        camera.position.z -= forwardSpeed;
+    }
+  };
+
+  function setDirectionOnEventKeyPress(event) {
+    direction = event.code
   }
 
   return <div className="App" id="container" />;
